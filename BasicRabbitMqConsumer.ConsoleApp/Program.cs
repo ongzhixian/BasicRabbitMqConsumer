f6b4195ef256681018f7a5e17bdbe5c1ea12f7fa
zhixian <zhixian@hotmail.com>
2022-05-15 09:20:16 +0800

    init commit

---
+++ BasicRabbitMqConsumer.ConsoleApp/Program.cs
@@ -0,0 +1,56 @@
+﻿using System.Reflection;
+using System.Text;
+using Microsoft.Extensions.Configuration;
+using RabbitMQ.Client;
+using RabbitMQ.Client.Events;
+
+const string RABBITMQ_URL_CONFIGURATION_KEY = "rabbitmq:Url";
+
+IConfiguration configuration = new ConfigurationBuilder()
+    .AddUserSecrets(Assembly.GetExecutingAssembly(), true)
+    .Build();
+
+var rabbitMQUrl = configuration[RABBITMQ_URL_CONFIGURATION_KEY];
+
+var exitEvent = new ManualResetEvent(false);
+
+Console.WriteLine("Press <CTRL-C> to terminate application.");
+
+Console.CancelKeyPress += (sender, eventArgs) =>
+{
+    eventArgs.Cancel = true;
+    exitEvent.Set();
+};
+
+var connectionFactory = new ConnectionFactory()
+{
+    Uri = new Uri(rabbitMQUrl)
+};
+
+using(var connection = connectionFactory.CreateConnection())
+using(var channel = connection.CreateModel())
+{
+    channel.QueueDeclare(queue: "hello",
+                            durable: false,
+                            exclusive: false,
+                            autoDelete: false,
+                            arguments: null);
+
+    var consumer = new EventingBasicConsumer(channel);
+
+    consumer.Received += (model, ea) =>
+    {
+        var body = ea.Body.ToArray();
+        var message = Encoding.UTF8.GetString(body);
+        Console.WriteLine(" [x] Received {0}", message);
+    };
+
+    Console.WriteLine("Consuming queue");
+
+    channel.BasicConsume(
+        queue: "hello",
+        autoAck: true,
+        consumer: consumer);
+
+    exitEvent.WaitOne();
+}
--- README.md
+++ README.md
@@ -1,2 +1,31 @@
 # BasicRabbitMqConsumer
+
 A basic .NET Core RabbitMQ consumer console application use for simple deployments in Kubernetes.
+
+## dotnet CLI
+
+dotnet CLI used to create this project:
+
+```ps1: In C:\src\github.com\ongzhixian\BasicRabbitMqConsumer
+dotnet new sln -n BasicRabbitMqConsumer
+dotnet new console -n BasicRabbitMqConsumer.ConsoleApp
+dotnet sln .\BasicRabbitMqConsumer.sln add .\BasicRabbitMqConsumer.ConsoleApp\
+
+dotnet add .\BasicRabbitMqConsumer.ConsoleApp\ package Microsoft.Extensions.Configuration
+dotnet add .\BasicRabbitMqConsumer.ConsoleApp\ package Microsoft.Extensions.Configuration.UserSecrets
+
+dotnet add .\BasicRabbitMqConsumer.ConsoleApp\ package RabbitMQ.Client
+
+dotnet user-secrets --project .\BasicRabbitMqConsumer.ConsoleApp\ init
+dotnet user-secrets --project .\BasicRabbitMqConsumer.ConsoleApp\ set "rabbitmq:Url" "amqps://<username>:<password>@<server>/<instance>"
+
+```
+
+Other ways to extend configuration
+
+```
+Microsoft.Extensions.Configuration.Json
+Microsoft.Extensions.Configuration.CommandLine 
+Microsoft.Extensions.Configuration.Binder 
+Microsoft.Extensions.Configuration.EnvironmentVariables
+```
